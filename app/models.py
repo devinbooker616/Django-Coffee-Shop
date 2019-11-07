d6cd1c63e01d04afb8eefec476e269e1aecf98cb
Base Camp <basecamp@ssa-406.local>
2019-11-07 14:40:13 -0600

    added models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Coffee(models.Model):
+    name = models.TextField()
+    price = models.FloatField()
+
+class Transaction(models.Model):
+    time = models.DateTimeField()
+    item = models.ForeignKey(Coffee, on_delete=models.CASCADE)
+    pre_tax = models.FloatField()
+    tax = models.FloatField()
